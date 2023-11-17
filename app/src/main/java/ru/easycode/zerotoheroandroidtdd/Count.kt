3d6fbf5a33d570f9c75d8ac45cdd36762dc6d1e8
DaddyShark97 <shumikdenys@gmail.com>
2023-11-17 18:56:24 +0200

    task/011 solved

---
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/Count.kt
@@ -0,0 +1,26 @@
+package ru.easycode.zerotoheroandroidtdd
+
+import java.lang.IllegalStateException
+
+
+interface Count {
+    fun increment(number: String): String
+
+
+    class Base(private val step: Int) : Count {
+
+        init {
+            if (step < 1)
+                throw IllegalStateException("Step should be positive but was $step")
+        }
+
+        override fun increment(number: String): String {
+            val digits = number.toInt()
+            val result = digits + step
+
+            return result.toString()
+
+
+        }
+    }
+}
