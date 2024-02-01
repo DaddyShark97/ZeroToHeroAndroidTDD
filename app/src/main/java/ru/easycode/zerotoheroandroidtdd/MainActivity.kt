4ab7faa2605d35c5f55bb17a5daf9c5e0ddfc5e7
Denny <shumikdenys@gmail.com>
2024-02-01 11:57:17 +0200

    task/024 solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -6,11 +6,63 @@ import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var viewModel: MainViewModel
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        viewModel = (application as App).viewModel
+
+        val adapter = Adapter()
+        binding.recyclerView.adapter = adapter
+        binding.actionButton.setOnClickListener {
+            val text = binding.inputEditText.text.toString()
+            viewModel.add(text)
+            binding.inputEditText.setText("")
+        }
+
+        viewModel.liveData().observe(this) {
+            adapter.update(it)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        viewModel.save(BundleWrapper.Base(outState))
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        viewModel.restore(BundleWrapper.Base(savedInstanceState))
     }
 }
+
+//interface NotNullSomething<T : Any> {
+//    fun show(source:T)
+//}
+//
+//interface NullableSomething<T> {
+//    fun showNullable(source: T)
+//}
+//
+//interface ShowNullableString : NullableSomething<String?>{
+//
+//    class Base : ShowNullableString {
+//        override fun showNullable(source: String?) {
+//            TODO("Not yet implemented")
+//        }
+//    }
+//}
+//
+//interface ShowString: NotNullSomething<String> {
+//
+//    class Base : ShowString {
+//
+//        override fun show(source: String) {
+//            TODO("Not yet implemented")
+//        }
+//    }
+//}
