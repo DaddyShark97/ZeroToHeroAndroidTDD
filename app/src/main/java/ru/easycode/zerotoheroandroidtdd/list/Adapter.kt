edf951c3acc8c97c014ef71ad0c0f5ca5359997f
Denny <shumikdenys@gmail.com>
2024-02-02 13:47:03 +0200

    task/025 solved //TODO FIX

---
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/list/Adapter.kt
@@ -0,0 +1,57 @@
+package ru.easycode.zerotoheroandroidtdd
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.appcompat.view.menu.MenuView.ItemView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import ru.easycode.zerotoheroandroidtdd.databinding.ItemLayoutBinding
+
+class Adapter : RecyclerView.Adapter<ItemViewHolder>() {
+
+    private val list = ArrayList<CharSequence>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        return ItemViewHolder(ItemLayoutBinding.inflate(LayoutInflater.from(parent.context)))
+    }
+
+    override fun getItemCount() = list.size
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        holder.bind(list[position])
+    }
+
+    fun update(newList: List<CharSequence>) {
+        val diff = DiffUtilCallback(list, newList)
+        val result = DiffUtil.calculateDiff(diff)
+        list.clear()
+        list.addAll(newList)
+        result.dispatchUpdatesTo(this)
+    }
+}
+
+class ItemViewHolder(private val binding: ItemLayoutBinding): ViewHolder(binding.root) {
+
+    fun bind(text: CharSequence) {
+        binding.elementTextView.text = text
+    }
+}
+
+private class DiffUtilCallback(
+    private val old: List<CharSequence>,
+    private val new: List<CharSequence>
+) : DiffUtil.Callback() {
+
+    override fun getOldListSize() = old.size
+
+    override fun getNewListSize() = new.size
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        return old[oldItemPosition] == new[newItemPosition]
+    }
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        return old[oldItemPosition] == new[newItemPosition]
+    }
+}
---
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/list/ListScreen.kt
@@ -0,0 +1,7 @@
+package ru.easycode.zerotoheroandroidtdd.list
+
+import androidx.fragment.app.ListFragment
+import ru.easycode.zerotoheroandroidtdd.main.Screen
+
+object ListScreen : Screen.Replace(ListFragment::class.java) {
+}
