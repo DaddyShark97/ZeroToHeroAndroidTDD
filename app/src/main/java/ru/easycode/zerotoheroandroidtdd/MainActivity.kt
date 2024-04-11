caafed86fc38e3270c6b700f51701a0c2f9a5a10
Denny <shumikdenys@gmail.com>
2024-04-11 13:05:08 +0300

    resolved task/006

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,23 @@
 package ru.easycode.zerotoheroandroidtdd
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView : TextView = findViewById(R.id.titleTextView)
+        val button : Button = findViewById(R.id.changeButton)
+
+        button.setOnClickListener{
+
+            textView.text = "I am an Android Developer!"
+            textView.setBackgroundColor(Color.GREEN)
+        }
     }
 }
